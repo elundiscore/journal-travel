a26d5f169847dbbd2168ab597b4995c1ac866c0c
f.ramzan@surreyschools.ca <f.ramzan@surreyschools.ca>
2023-01-16 10:53:12 -0800

    starting travel journal react portion

---
+++ frontend/src/components/header.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+    <div>
+        <nav className="navbar">
+            <p className="navbar--text">Travel Journal</p>
+            <p className="navbar--options">Login</p>
+            <p className="navbar--options">Signup</p>
+        </nav>
+    </div>
+    )
+}
